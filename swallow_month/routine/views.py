209f23a44e609b36c9b5723735ec46fca96f33f3
jinuemong <jinwoo3661@naver.com>
2023-01-11 11:20:59 +0900

    20230111

--- swallow_month/routine/views.py
+++ swallow_month/routine/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Routine,DayRoutine
+from .Serializer import RoutineSerializer,DayRoutineSerializer
+from rest_framework import viewsets
+from rest_framework import filters
+
+class RoutineViewSet(viewsets.ModelViewSet):
+    
+    queryset = Routine.objects.all()
+    serializer_class = RoutineSerializer
+    filter_backends = [filters.SearchFilter]
+    # search_fields =[]
+
+class DayRoutineViewSet(viewsets.ModelViewSet):
+    
+    queryset = DayRoutine.objects.all()
+    serializer_class = DayRoutineSerializer
+    filter_backends = [filters.SearchFilter]
+    # search_fields =[]
--- swallow_month/swallow_month/urls.py
+++ swallow_month/swallow_month/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from dayData import urls
+from routine import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('daydata/',include('dayData.urls'))
+    path('daydata/',include('dayData.urls')),
+    path('routine/',include('routine.urls'))
 ]
